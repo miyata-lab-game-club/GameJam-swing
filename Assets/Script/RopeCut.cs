384d335f1a4379e53cfe192024b2862dc01b1a88
yamarico <cssu19093@g.nihon-u.ac.jp>
2023-06-25 02:08:49 +0900

    吊り下げギミック完成

---
+++ Assets/Script/RopeCut.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RopeCut : MonoBehaviour
+{
+    public HingeJoint2D hinge; // ヒンジジョイント
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Arrow")) // 矢が当たったら
+        {
+            Destroy(hinge); // ヒンジジョイントを削除
+            Destroy(collision.gameObject); // 矢を消す
+        }
+    }
+}
